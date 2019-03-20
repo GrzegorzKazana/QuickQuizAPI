418bafc50d26b54384384c6708d46313b9f5a669
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-03-20 16:45:55 +0100

    updated throttle rates

--- quiz/throttles.py
+++ quiz/throttles.py
@@ -4,7 +4,3 @@ from rest_framework.exceptions import Throttled
 
 class QuizPostThrottle(ScopedRateThrottle):
     scope_attr = 'quiz_post_throttle_scope'
-
-
-class QuizGetThrottle(ScopedRateThrottle):
-    scope_attr = 'quiz_get_throttle_scope'
